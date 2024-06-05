8f0f43fad5519f416c9fc72e90b9d9677a368a1f
Benedikt <107284232+benwies@users.noreply.github.com>
2024-06-05 16:23:17 +0200

    updated code

--- code/binay.py
+++ code/binay.py
@@ -1,24 +1,22 @@
 def embed_text_in_existing_image(image_path, text, output_path):
-    """Embed plain text at the end of an existing image file."""
-    # Read the original image data
     with open(image_path, 'rb') as f:
         image_data = f.read()
     
-    # Convert the text to bytes and add a delimiter to mark the end of the text
+
     text_bytes = text.encode('utf-8') + b'\x00'
     
-    # Append the text bytes to the image data
+
     combined_data = image_data + text_bytes
     
-    # Save the new image file with the embedded text
+
     with open(output_path, 'wb') as f:
         f.write(combined_data)
     
     print(f"Text embedded in {output_path}")
 
-# Example usage
-image_path = 'input_image.png'  # Replace with the path to your uploaded image
-text = 'Q2FscHVybmlhIG9uZSBzYWlkIGt3d3N2Oi8vc2R2d2hlbHEuZnJwL01KcXM4VGt3=='  # Your text message
-output_path = 'output_image.png'  # Output file path
+
+image_path = 'input_image.png'  
+text = 'Q2FscHVybmlhIG9uZSBzYWlkIGt3d3N2Oi8vc2R2d2hlbHEuZnJwL01KcXM4VGt3=='  
+output_path = 'output_image.png'  
 
 embed_text_in_existing_image(image_path, text, output_path)
--- code/decode.py
+++ code/decode.py
@@ -25,7 +25,7 @@ Dgy, djatpi vmg agnn th yjj Ofvwkc nngx c igjrjt ytzvm—c ytzvm qguhwwgi dd nfa
 Fu yjj vmgttd wshtniu, nv gghmtpx wx vt szgxvnqs vmg scywwg th jznuygsej, vt rwqgg yjj dtwsfftngx qk etpxenqzusgxu, fpi vt etpkttpy vmg uqxundnnnvd vmcy qzt wgfnnvd ofa gg sqyjnpl ottj vmcs cs gqcgqwcyg nnqwxktp. Nv hjfnqgsiju zu yq qqtm ggdqsf yjj etpkksgx qk qzt ugwejkagi tjcqkya fpi vt iqkrrxg yjj kshnpnvj rtuxkgkqkykju yjfv qkj djatpi.     
 Dzv yjj Vmgttd qk vmg Rcytnz nu rqwg yjfp rgwg xrjeznfvnqs; ky kx c wgknjeyktp th tww fjgugxv kgftx csf igxkwgx, qzt dgftsksi kqw vwwyj np f yttqf th ighguvnqs csf zphgwvfksvd. Ky ksxnvju zu yq jogtfej vmg zppptys, vt xjpywwg npyq yjj fjryjx qk vmg zppptys, csf yq xgjm fpxyjtx vt vmg kwsffojpycq szgxvnqsu yjfv mcag unfizgi jzofpnvd unphg ykrg norgrqwkfn.
 Np yjj gsf, yjj Vmgttd qk vmg Rcytnz nu f ltwwpja—f ltwwpja npyq yjj wsmsqbp, npyq yjj fjryjx qk etpxenqzusgxu, fpi ksvt vmg agwa mgfty qk gckxvjphg nvxgqh. Nv nu f szgxv kqw vwwyj, kqw wsfjtxvfpiksi, fpi htt jpqkljygsojpy ks c bqwni umttwigi ks oduygwa fpi kqnzunqs."""
-    decrypted_text = caesar_decipher(text, 25) # assuming the key is 25 as given in the example
+    decrypted_text = caesar_decipher(text, 25)
     print("Decrypted text:", decrypted_text)
 
 if __name__ == "__main__":
